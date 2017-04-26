b9ab3d0b4ede205cc27483e6a12961283d54f898
jonmid <jamideros@hotmail.com>
2017-04-26 13:57:59 -0500

    Initial commit parcial2

---
+++ app/src/main/java/com/jonmid/segundoparcial/Parser/Json.java
@@ -0,0 +1,35 @@
+package com.jonmid.segundoparcial.Parser;
+
+import com.jonmid.segundoparcial.Models.User;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Json {
+
+    public static List<User> parserJsonUser(String content) throws Exception {
+        JSONArray myArray = new JSONArray(content);
+        List<User> myUser = new ArrayList<>();
+
+        for (int i=0; i<myArray.length(); i++){
+            JSONObject item = myArray.getJSONObject(i);
+
+
+            User usuario = new User();
+            usuario.setUsername(item.getString("username"));
+            usuario.setPhone(item.getString("phone"));
+            usuario.setWebsite(item.getString("email"));
+
+            myUser.add(usuario);
+
+
+
+        }
+
+        return myUser;
+    }
+
+}
